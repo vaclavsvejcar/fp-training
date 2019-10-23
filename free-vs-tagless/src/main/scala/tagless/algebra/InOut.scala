46cb55206fe01cc3ef06c0280ba91ba444ed4b8b
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-10-23 09:05:58 +0200

    minor cleanup

--- free-vs-tagless/src/main/scala/tagless/algebra/InOut.scala
+++ free-vs-tagless/src/main/scala/tagless/algebra/InOut.scala
@@ -3,7 +3,3 @@ package tagless.algebra
 trait InOut[F[_]] {
   def printLine(line: String): F[Unit]
 }
-
-object InOut {
-  def apply[F[_]](implicit I: InOut[F]): InOut[F] = I
-}
--- free-vs-tagless/src/main/scala/tagless/algebra/Log.scala
+++ free-vs-tagless/src/main/scala/tagless/algebra/Log.scala
@@ -3,7 +3,3 @@ package tagless.algebra
 trait Log[F[_]] {
   def info(message: String): F[Unit]
 }
-
-object Log {
-  def apply[F[_]](implicit I: Log[F]): Log[F] = I
-}
