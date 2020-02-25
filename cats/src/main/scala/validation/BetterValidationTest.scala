de09c6bf1c67d8e05f60f1909588a3800230373b
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-02-25 10:53:16 +0100

    Examples changes

--- cats/src/main/scala/validation/BetterValidationTest.scala
+++ cats/src/main/scala/validation/BetterValidationTest.scala
@@ -2,17 +2,6 @@ package validation
 
 import cats.data.Validated
 
-object EitherValidationTest extends App {
-
-  val result: Either[Domain.FormError, Domain.Form] =
-    EitherValidation.validateForm("user#$", "SuperTajneHeslo", "John", "Smith", 42)
-
-  result match {
-    case Left(error) => println(s"ERROR: ${error.msg}")
-    case Right(form) => println(s"SUCCESS: $form")
-  }
-}
-
 object BetterValidationTest extends App {
   val result: BetterValidation.ValidationResult[Domain.Form] =
     BetterValidation.validateForm("user#$", "SuperTajneHeslo", "John", "Smith", 42)
---
+++ cats/src/main/scala/validation/EitherValidationTest.scala
@@ -0,0 +1,12 @@
+package validation
+
+object EitherValidationTest extends App {
+
+  val result: Either[Domain.FormError, Domain.Form] =
+    EitherValidation.validateForm("user#$", "SuperTajneHeslo", "John", "Smith", 42)
+
+  result match {
+    case Left(error) => println(s"ERROR: ${error.msg}")
+    case Right(form) => println(s"SUCCESS: $form")
+  }
+}
